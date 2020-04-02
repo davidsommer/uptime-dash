afdf16c74da1b77834bb526c48d4bd9c141ef89e
David Sommer <dsommer@fluance.ch>
2020-04-02 15:05:43 +0200

    removed key

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 //array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-		'u102136-92acc35c0cbe7a1bd2bc9848' //TODO set your API
+		'' //TODO set your API
 	];
 
 //refresh interval (in seconds)
