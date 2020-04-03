a2e16005392e8fa8134f6f0bae2be3c8eb1cdc71
David Sommer <dsommer@fluance.ch>
2020-04-03 08:44:17 +0200

    error handling for API

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 //array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-		'ur102136-6db423c2ca1b5bbc24eb817d' //TODO set your API
+		'' //TODO set your API
 	];
 
 //refresh interval (in seconds)
